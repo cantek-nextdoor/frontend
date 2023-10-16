ce0ff45cea17c7b32c063cbe8dd4fa808752242f
Lulu Tung <lulutheflaneur@gmail.com>
2023-10-15 22:43:04 -0400

    Set up api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+
+export default ({mode}) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: process.env.VITE_SERVER_URL,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        }
+    });
+}
