7768766a3ef9f2d0b6f93febf08113f92234a6c8
Jordan Lenard David <jordanlenard.david@gmail.com>
2023-10-24 16:06:56 -0400

    [+] Added validation for login

--- src/components/AuthPanel.tsx
+++ src/components/AuthPanel.tsx
@@ -11,13 +11,24 @@ import {useNavigate} from "react-router-dom";
 import {loginUser, registerUser} from "../axios/auth.ts";
 import {TextFieldMui} from "../ui-components/TextFieldMui.tsx";
 import {useUserStore} from "../zustand/user.ts";
+import { FormHelperText } from "@mui/material";
 
 export const AuthPanel = () => {
     const [isLogin, setIsLogin] = useState(true);
+    const [password, setPassword]  = useState("");
+    const [errorMessage, setError] = useState("");
 
     const navigate = useNavigate();
     const ACTION_MESSAGE = isLogin ? "Sign In" : "Sign Up";
 
+    const enum errorMessages {
+        WRONG_EMAIL = "Wrong Email Address",
+        MISMATCH_PWD = "Passwords are not the same",
+        INVALID_POSTAL_CODE = "Postal Code is invalid, use A#A #A#",
+        NOT_AUTHORIZED = "Email or Password is invalid",
+        REQUIRED = "Please fill up required fields"
+    }
+    
     const updateUser = useUserStore((state) => state.updateUser)
 
     const Copyright = () => {
@@ -38,21 +49,88 @@ export const AuthPanel = () => {
 
     const handleToggleAuth = () => {
         setIsLogin((prev) => !prev);
+        if (errorMessage != errorMessages.WRONG_EMAIL)
+        {
+            // cleanup error message for invalid postal code and mismatch pwd
+            setError("");
+        }
     };
 
-    // TODO add validation
+    // Check if email is valid format during onChange
+    const validateEmail = (input:string) => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(input))
+        {
+            setError(errorMessages.WRONG_EMAIL);
+            return;
+        }
+        setError("");
+    }
+
+    // Check if postal code is valid format during onChange
+    const validatePostalCode = (input:string) => {
+        const canadianPostalCodeRegex = /^[A-Z]\d[A-Z] \d[A-Z]\d$/;
+        if (!canadianPostalCodeRegex.test(input))
+        {
+            setError(errorMessages.INVALID_POSTAL_CODE);
+            return;
+        }
+        setError("");
+    }
+
+    // Check if password and confirm password are the same
+    const validatePassword = (input:string) => {
+        if(password != input)
+        {
+            setError(errorMessages.MISMATCH_PWD);
+            return;
+        }
+        setError("");
+    }
+
+    // Store Password on state for checking (during registration)
+    const savePassword = (input:string) => {
+        setPassword(input);
+    }
+
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
+        const inputEmail = formData.get("email")!.toString();
+        const inputPassword = formData.get("password")!.toString();
+        
+        if(inputEmail == "" || inputPassword == "")
+        {
+            setError(errorMessages.REQUIRED);
+            return;
+        }
+        if (errorMessage != "")
+        {
+            return;
+        }
         const payload = {
-            email: formData.get("email")!.toString(),
-            password: formData.get("password")!.toString(),
+            email: inputEmail,
+            password: inputPassword
         }
         const registerPayload = {
             ...payload,
             postalCode: formData.get("postal_code")?.toString(),
         }
+        if(!isLogin)
+        {
+            const confirmPwd = formData.get("confirm_password")?.toString();
+            if(registerPayload.postalCode == "" || confirmPwd == undefined || confirmPwd == "")
+            {
+                setError(errorMessages.REQUIRED);
+                return;
+            }
+        }
         const res = isLogin ? await loginUser(payload) : await registerUser(registerPayload);
+        if(res == null)
+        {
+            setError("Login Failed");
+            return;
+        }
         updateUser({...res.data, isLoggedIn: true})
         navigate("/");
     };
@@ -77,13 +155,15 @@ export const AuthPanel = () => {
                 </Typography>
                 <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
 
-                    <TextFieldMui autoComplete="email" autoFocus label="Email Address" name="email" required
-                                  size="medium"/>
-                    <TextFieldMui autoComplete="current-password" label="Password" name="password" type="password"
-                                  required size="medium"/>
+                    <TextFieldMui autoComplete="email" error={errorMessage==errorMessages.WRONG_EMAIL || errorMessage==errorMessages.REQUIRED } autoFocus label="Email Address" name="email" required
+                                  size="medium" onChange={(e)=>validateEmail(e.target.value)}/>
+                    <TextFieldMui autoComplete="current-password"  error={errorMessage==errorMessages.REQUIRED } label="Password" name="password" type="password"
+                                  required size="medium" onChange={(e)=>savePassword(e.target.value)} />
+                    {!isLogin && <TextFieldMui autoComplete="confirm-password" label="Confirm Password" name="confirmPassword" type="password"
+                                  error={errorMessage==errorMessages.MISMATCH_PWD || errorMessage==errorMessages.REQUIRED} required size="medium" onChange={(e)=>validatePassword(e.target.value)}/>}
                     {!isLogin && <TextFieldMui autoComplete="postal-code" label="Postal Code" name="postal_code"
-                                               required size="medium"/>}
-
+                                  error={errorMessage==errorMessages.INVALID_POSTAL_CODE || errorMessage==errorMessages.REQUIRED} required size="medium" onChange={(e)=>validatePostalCode(e.target.value)}/>}
+                    {errorMessage && <FormHelperText error>{errorMessage}</FormHelperText>}
                     {/*<FormControlLabel*/}
                     {/*  control={<Checkbox value="remember" color="primary" />}*/}
                     {/*  label="Remember me"*/}
--- src/ui-components/ButtonMui.tsx
+++ src/ui-components/ButtonMui.tsx
@@ -2,19 +2,20 @@ import { Button, ButtonProps } from "@mui/material";
 import { NavLinkProps } from "react-router-dom";
 
 type TButtonMuiProps =
-  | ButtonProps<"button", { text: string }>
+  | ButtonProps<"button", { text: string, disabled?:boolean }>
   | (ButtonProps<
       "button",
       {
         component: React.ComponentType<NavLinkProps>;
         text: string;
+        disabled?: boolean;
       }
     > &
       NavLinkProps);
 
-const ButtonMui = ({ text, ...others }: TButtonMuiProps) => {
+const ButtonMui = ({ text, disabled=false, ...others }: TButtonMuiProps) => {
   return (
-    <Button color="primary" variant="text" {...others}>
+    <Button color="primary" disabled={disabled} variant="text" {...others}>
       {text}
     </Button>
   );
